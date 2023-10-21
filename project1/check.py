addb5874b6e0d794bd70c29047587231af35de01
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2023-10-21 14:21:17 -0700

    Add testing script

---
+++ project1/check.py
@@ -0,0 +1,32 @@
+import subprocess, textwrap
+
+TESTS = [
+    # Filename, expected output
+    ('23instMem-r.txt', b'(-8,23)\n'),
+    ('23instMem-sw.txt', b'(9,17)\n'),
+    ('23instMem-all.txt', b'(40,1)\n'),
+]
+EXECUTABLE_PATH = './cpusim'
+TESTS_DIRECTORY = 'trace/'
+
+def main():
+    n_passed = 0
+    for filename, expected in TESTS:
+        result = subprocess.run([EXECUTABLE_PATH, f'{TESTS_DIRECTORY}{filename}'], stdout=subprocess.PIPE)
+        if result.stdout == expected:
+            print(f'========== {filename} ✅')
+            n_passed += 1
+        else:
+            print(textwrap.dedent(f'''
+                ========== {filename} ❌
+                Expected: {expected.decode()}
+                Received: {result.stdout.decode()}'''),
+                end='')
+    print(f'===== {n_passed}/{len(TESTS)} tests passed')
+    if n_passed == len(TESTS):
+        exit(0)
+    else:
+        exit(1)
+
+if __name__ == '__main__':
+    main()
